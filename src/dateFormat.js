6b034456e7ad30f34c21dabdba1efaa61b25dc67
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-09-20 18:10:16 -0400

    fix: local date

--- src/dateFormat.js
+++ src/dateFormat.js
@@ -1,4 +1,5 @@
 export default function dateFormat(date) {
-  date = date.split(' ')[1];
-  return date.slice(0, date.length - 3);
+  date = new Date(date.replace(' ', 'T') + 'Z');
+  date = date.toString().split(' ')[4];
+  return date.slice(0, date.length - 3).replace(':','h');
 }
