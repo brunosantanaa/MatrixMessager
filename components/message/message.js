e3576c2ad9b971a098d0c3acd9e13397872eaff4
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-09-20 20:22:24 -0400

    feat: name colors

--- components/message/message.js
+++ components/message/message.js
@@ -4,12 +4,19 @@ import { div, p, h4, import_link } from '../../src/html.js';
 import_link('./components/message/message.css');
 
 export default function Message(props){
+  let rgbName=[];
+  for(let i=0; i < 3; i++) {
+    rgbName.push(props.name.charCodeAt(i));
+  }
+  rgbName[props.User % 3] = 255;
   return (
     div({
       class: 'message-container message-'+(props.who != 'moi'? 'general' : 'moi'),
       content: [
         div({class: 'message-info', content: [
-          p({class: 'message-name', content: props.name}),
+          p({class: 'message-name', 
+            style: (props.who != 'moi'? 'color: rgb('+rgbName[0]+','+rgbName[1]+','+rgbName[2]+') !important;' : ''), 
+            content: (props.who != 'moi'? props.name : 'Vous')}),
           p({class: 'message-time', content: dateFormat(props.Date)})
         ]}),
         p({class: 'message-content', content: props.Content}),
